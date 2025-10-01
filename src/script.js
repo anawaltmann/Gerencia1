f8f467a77a5a4251855acb5ea319f047cdc441a0
yslucass <lucas.suguimoto@estudante.ifms.edu.br>
2025-10-01 15:12:11 -0400

    Update index.html and style.css

--- src/script.js
+++ src/script.js
@@ -1,3 +1,11 @@
-function mostrarMensagem() {
-  alert("Você clicou no botão do card!");
-}
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll('.card');
+
+    setTimeout(() => {
+      cards.forEach(card => {
+        card.classList.remove('animate__animated', 'animate__fadeInUp');
+      });
+    }, 1000); // remove animação após 1 segundo
+  });
+</script>
